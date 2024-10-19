f4fb40d7a9ee8b86ddfc159e82f860420a25e9d9
sunzhangxu <pausermake@126.com>
2024-10-19 15:35:00 +0800

    xxxxx

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/GIT_USER_ID/GIT_REPO_ID
+module github.com/pausermake/go-zinc-sdk
 
 go 1.18
 
--- test/api_user_test.go
+++ test/api_user_test.go
@@ -11,7 +11,7 @@ package openapi
 
 import (
 	"context"
-	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
+	openapiclient "github.com/pausermake/go-zinc-sdk"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
